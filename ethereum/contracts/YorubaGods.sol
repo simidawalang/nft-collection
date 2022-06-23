61984ff558fd77a660cb31087df0da944b1d9598
simidawalang <simidawalang@gmail.com>
2022-06-23 11:28:24 +0100

    adjusted YorubaGods.sol

--- ethereum/contracts/YorubaGods.sol
+++ ethereum/contracts/YorubaGods.sol
@@ -7,64 +7,65 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IWhitelist.sol";
 
 contract YorubaGods is ERC721Enumerable, Ownable {
-    string _baseTokenURI;
-    uint public _price = 0.01 ether;
-    uint public tokenIds;
-    uint public maxTokenIds = 20;
-    uint public presaleEnded;
-    bool public presaleStarted;
-    bool public _paused;
+      string _baseTokenURI;
+      uint256 public _price = 0.01 ether;
+      bool public _paused;
+      uint256 public maxTokenIds = 20;
+      uint256 public tokenIds;
+      IWhitelist whitelist;
+      bool public presaleStarted;
+      uint256 public presaleEnded;
 
-    IWhitelist whitelist;
+      modifier onlyWhenNotPaused {
+          require(!_paused, "Contract currently paused");
+          _;
+      }
 
-    modifier onlyWhenNotPaused {
-        require(!_paused, "This contract has been temporarily paused.");
-        _;
-    }
+      constructor (string memory baseURI, address whitelistContract) ERC721("YorubaGods", "YG") {
+          _baseTokenURI = baseURI;
+          whitelist = IWhitelist(whitelistContract);
+      }
 
-    constructor(string memory baseURI, address whitelistContract) ERC721("Yoruba Gods", "YBG"){
-        _baseTokenURI = baseURI;
-        whitelist = IWhitelist(whitelistContract);
-    }
+      function startPresale() public onlyOwner {
+          presaleStarted = true;
+          presaleEnded = block.timestamp + 5 minutes;
+      }
 
-    function startPresale() public onlyOwner {
-        presaleStarted = true;
-        presaleEnded = block.timestamp + 5 minutes;
-    }
+      function presaleMint() public payable onlyWhenNotPaused {
+          require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+          require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+          require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
+          require(msg.value >= _price, "Ether sent is not correct");
+          tokenIds += 1;
 
-    function presaleMint() public payable onlyWhenNotPaused {
-        require(presaleStarted && block.timestamp < presaleEnded, "The presale hasn't started yet.");
-        require(whitelist.whitelistedAddresses(msg.sender), "This address was not whitelisted.");
-        require(tokenIds < maxTokenIds, "Maximum supply reached");
-        require(msg.value >= _price, "Send exactly 0.01 ether");
+          _safeMint(msg.sender, tokenIds);
+      }
 
-        tokenIds++;
 
-        _safeMint(msg.sender, tokenIds);
-    }
+      function mint() public payable onlyWhenNotPaused {
+          require(presaleStarted && block.timestamp >=  presaleEnded, "Presale hasn't ended yet.");
+          require(tokenIds < maxTokenIds, "Maximum Yoruba Gods supply reaches.");
+          require(msg.value >= _price, "Send exactly 0,01 ether");
+          tokenIds += 1;
+          _safeMint(msg.sender, tokenIds);
+      }
 
-    function mint() public payable onlyWhenNotPaused {
-        require(presaleStarted && block.timestamp > presaleEnded, "The presale hasn't ended yet");
-        require(tokenIds < maxTokenIds, "Max supply reached.");
-        require(msg.value >= _price, "Ether sent is not correct");
-          
-        tokenIds++;
-    }
-   function _baseURI() internal view virtual override returns (string memory) {
+      
+      function _baseURI() internal view virtual override returns (string memory) {
           return _baseTokenURI;
       }
-    function setPaused(bool val) public onlyOwner {
-        _paused = val;
-    }
+     
+      function setPaused(bool val) public onlyOwner {
+          _paused = val;
+      }
 
-    function withdraw() public onlyOwner {
-        address _owner = owner();
-        uint amount = address(this).balance;
-        (bool sent,) = _owner.call{value: amount}("");
-        require(sent, "Failed to send ether.");
-    }
+      function withdraw() public onlyOwner  {
+          address _owner = owner();
+          uint256 amount = address(this).balance;
+          (bool sent, ) =  _owner.call{value: amount}("");
+          require(sent, "Failed to send Ether");
+      }
 
-    receive() external payable {}
-
-    fallback() external payable {}
-}
+      receive() external payable {}
+      fallback() external payable {}
+  }
